e845d72ca1c513fe1d24fba8bcc6ca79fc74869a
Alessandro Visotto <visotto@users.noreply.github.com>
2014-06-06 20:18:16 -0300

    T2 e T3 versão final

--- Caminho de Maior Soma no Triangulo/HP.c
+++ Caminho de Maior Soma no Triangulo/HP.c
@@ -1,35 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> // entrada e saida
+#include <stdlib.h> // alocacao dinamica
 
 int main() {
-  int N; // 
-  long int **matriz;
-  int i, j;
-  
-  matriz = malloc(5000 * sizeof(long int*));
-  for(i=0; i<5000; i++){
-    matriz[i] = malloc((i+1) * sizeof(long int));
+  int N; // qtd de linhas/niveis
+  int** triangulo; // array de int*, 2 dimensoes
+  int i, j; // indices
+
+  triangulo = malloc(5000 * sizeof(int*)); // cria as linhas/niveis
+  for (i = 0; i < 5000; i++) { // cria as colunas
+    triangulo[i] = malloc((i + 1) * sizeof(int)); // i+1 = a cada linha/nivel o numero de colunas aumenta em 1
   }
-    
-  while(scanf("%d", &N) && N){
-    for(i=0; i<N; i++)
-      for(j=0; j<i+1; j++)
-        scanf("%ld", &matriz[i][j]);
-    
-    for(i=N-2; i>=0; i--) // Ultima linha eh n-1, soh que sao folha, entao comeco do n-2
-      for(j=0; j < i+1; j++)
-        if(matriz[i+1][j] > matriz[i+1][j+1]) // linha de baixo, coluna de baixo e coluna de baixo mais um
-          matriz[i][j] += matriz[i+1][j];
+
+  while (scanf("%d", &N) && N) { // N = 0 finaliza o programa
+
+    // preenche o triangulo
+    for (i = 0; i < N; i++)
+      for (j = 0; j < i + 1; j++)
+        scanf("%d", &triangulo[i][j]);
+
+    // verificacao de maior caminho comeca de baixo para cima
+    // verifica o maior entre direita e esquerda e soma com seu antecessor
+    for (i = N - 2; i >= 0; i--) // ultima linha eh n-1. Porem como eh a base, comeco de n-2
+      for (j = 0; j < i + 1; j++)
+        if (triangulo[i + 1][j] > triangulo[i + 1][j + 1]) // linha de baixo, coluna de baixo e coluna de baixo mais um
+          triangulo[i][j] += triangulo[i + 1][j]; // elemento da esquerda eh maior que o da direita
         else
-          matriz[i][j] += matriz[i+1][j+1];
+          triangulo[i][j] += triangulo[i + 1][j + 1]; // elemento da direita eh maior que o da esquerda
 
-    printf("%ld\n", matriz[0][0]);
+    printf("%d\n", triangulo[0][0]); // topo do triangulo, caminho de maior soma
 
   }
-  
-  for (i=0; i<5000; i++)
-    free(matriz[i]);
-  free(matriz);
-  
+
+  // libera as colunas de cada uma das linhas
+  for (i = 0; i < 5000; i++)
+    free(triangulo[i]);
+
+  free(triangulo);
+
   return 0;
 }
--- Maior Elemento de 3 Conjuntos/pescaria.c
+++ Maior Elemento de 3 Conjuntos/pescaria.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
+#include <stdio.h> // entrada e saida
 
-int main(int argc, char *argv[]) {
-  int N, // numero de peixinhos
-  X, // soma a ser obtida
-  i, // indice p/ os valores dos peixes
-  j, // indice p/ piscinas
-  temp, // armazena temporareamente o valor asociado de cada peixe
-  max, // peixe de maior valor
-  soma; // soma dos valores dos maiores peixes
-  
-  while (scanf("%d %d", &N, &X) && (N || X)) {
+int main() {
+  int N, // qtd de peixes
+      X, // soma a ser obtida
+      i, // indice p/ os valores dos peixes
+      j, // indice p/ piscinas
+      temp, // armazena temporareamente o valor associado de cada peixe
+      max, // peixe de maior valor
+      soma; // soma dos valores dos maiores peixes
+
+  while (scanf("%d %d", &N, &X) && (N || X)) { // N = X = 0 finaliza o programa
 
     soma = 0;
-  
-    for(j = 0; j < 3; j++) { // itera para 3 piscinas
-      max = -300-1; // minimo valor possivel de cada peixe - 1
+
+    for (j = 0; j < 3; j++) { // itera para 3 piscinas
+      max = -300 - 1; // minimo valor possivel de cada peixe - 1
       for (i = 0; i < N; i++) { // recebe o valor de cada peixe da piscina j e atualiza o maior valor do peixe
         scanf("%d", &temp);
-        if(temp > max)
+        if (temp > max)
           max = temp;
       }
       soma += max; // acumula na soma o maior valor do peixe na piscina j
     }
-    
+
     if (soma >= X) // valor acumulado maior ou igual ao premio
-        printf("SIM\n");
+      printf("SIM\n");
     else
-        printf("NÃO\n");
+      printf("NÃO\n");
   }
 
   return 0;
